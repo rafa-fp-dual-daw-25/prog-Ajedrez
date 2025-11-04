18c761a2b9a6013d7f21033e05f7b7f653019aa1
Taller_INF9e29 <rafamendoza.dev@gmail.com>
2025-11-04 17:19:00 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
 
     }
-
 
     public static int[][] movimientoCaballo(int fila, int columna) {
         int[][] tablero = new int[8][8];
@@ -32,33 +30,31 @@ public class Main {
 
     public static int[][] movimientoAlfil(int fila, int columna) {
         int[][] tablero = new int[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (fila )
+            }
+
+        }
+
         return tablero;
     }
 
     public static int[][] movimientoTorre(int fila, int columna) {
         int[][] tablero = new int[8][8];
-        int[][] movimientos = {
-                {1, 0},
-                {-1, 0},
-                {0, 1},
-                {0, -1}
-        };
 
-        for (int[] movimiento : movimientos) {
-            int nuevaFila = fila + movimiento[0];
-            int nuevaColumna = columna + movimiento[1];
-
-            if (nuevaFila >= 0 && nuevaFila < 8) {
-                for (int i = 0; i < tablero.length - 1; i++) {
-                }
+        for (int i = 0; i < 8; i++) {
+            if (i != fila) {
+                tablero[i][columna] = 1;
             }
-            (nuevaColumna >= 0 && nuevaColumna < 8)
-            tablero[nuevaFila][nuevaColumna] = 1;
-            tablero[nuevaFila][nuevaColumna] = 1;
         }
-        return tablero;
+
+        for (int j = 0; j < 8; j++) {
+            if (j != columna) {
+                tablero[fila][j] = 1;
+            }
+        }
+            return tablero;
     }
-}
-
-
 }
